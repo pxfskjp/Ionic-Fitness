9855b72cd7a710369bc1ebe58b7d7341299e254b
topdeveloper0729 <topdev0729@gmail.com>
2020-07-31 14:32:17 +1200

    Add change password Modal.

--- src/pages/settings/account/AccountPage.tsx
+++ src/pages/settings/account/AccountPage.tsx
@@ -1,9 +1,11 @@
-import React from 'react';
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import React, { useState } from 'react';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonModal, IonButton, IonItem, IonLabel, IonButtons } from '@ionic/react';
 import Header from '../../../components/settings/account/Header';
 import './AccountPage.css';
 
 const AccountPage: React.FC = () => {
+  const [showPasswordModal, setShowPasswordModal] = useState(false)
+
   return (
     <IonPage>
       <IonHeader>
@@ -17,8 +19,20 @@ const AccountPage: React.FC = () => {
             <IonTitle size="small">Account</IonTitle>
           </IonToolbar>
         </IonHeader>
-
-
+        <IonItem button onClick={() => setShowPasswordModal(true)} color="primary">
+            <IonLabel>Change Password</IonLabel>
+        </IonItem>
+        <IonModal isOpen={showPasswordModal}>
+          <IonHeader translucent>
+            <IonToolbar>
+              <IonTitle size="small">Change Password</IonTitle>
+              <IonButtons slot="end">
+                <IonButton onClick={() => setShowPasswordModal(false)}>Close</IonButton>
+              </IonButtons>
+            </IonToolbar>
+          </IonHeader>
+        </IonModal>
+        
       </IonContent>
     </IonPage>
   );
