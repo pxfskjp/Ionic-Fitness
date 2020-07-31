5f5d668feb046b2a758dd45f9d1c666b7605ed58
topdeveloper0729 <topdev0729@gmail.com>
2020-07-31 14:20:46 +1200

    Add logout alert

--- src/pages/settings/SettingsPage.tsx
+++ src/pages/settings/SettingsPage.tsx
@@ -1,10 +1,11 @@
-import React from 'react';
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonItem, IonLabel, IonToggle } from '@ionic/react';
+import React, { useState } from 'react';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonItem, IonLabel, IonToggle, IonAlert } from '@ionic/react';
 import './SettingsPage.css';
 import { useHistory } from 'react-router';
 
 const SettingsPage: React.FC = () => {
   const history = useHistory()
+  const [showLogout, setShowlogout] = useState(false)
 
   return (
     <IonPage>
@@ -26,6 +27,21 @@ const SettingsPage: React.FC = () => {
         <IonItem button onClick={() => history.push('/settings/account')} color="primary">
             <IonLabel>Account Settings</IonLabel>
         </IonItem>
+        <IonItem button onClick={() => setShowlogout(true)} color="danger">Log out</IonItem>
+        <IonAlert 
+          isOpen={showLogout}
+          onDidDismiss={() => setShowlogout(false)}
+          header="Logout"
+          message="Are you sure you want to logout?"
+          buttons={[
+            {
+              text:'Yes',
+              handler: () => {}
+            }, 
+            {
+              text:'No',
+              handler: () => {}
+            }]}/>
 
       </IonContent>
     </IonPage>
