605ec6acf3d47a4803588283123724f0ea0a7c7b
topdeveloper0729 <topdev0729@gmail.com>
2020-08-03 17:10:05 +1200

    Set post card header content as IonItem and add inline styles.

--- src/components/home/Post.tsx
+++ src/components/home/Post.tsx
@@ -1,32 +1,32 @@
 import React from 'react'
-import { IonCard, IonCardContent, IonCardHeader, IonIcon, IonCardSubtitle } from '@ionic/react'
-import {barbellOutline, bicycleOutline} from 'ionicons/icons';
+import { IonCard, IonCardContent, IonCardHeader, IonIcon, IonCardSubtitle, IonItem } from '@ionic/react'
+import { barbellOutline, bicycleOutline } from 'ionicons/icons';
 import { personCircleOutline } from 'ionicons/icons';
 import './Post.css'
 
 const Post: React.FC = () => {
     return (
         <IonCard>
-            <IonCardHeader>
-                <div className="post-header-container">
-                    <div className="post-header-icon">
-                        <IonIcon icon={personCircleOutline} size="large"></IonIcon>
+            <IonCardHeader style={{paddingLeft: "0px", paddingBottom: "0px"}}>
+                <IonItem>
+                    <div className="post-header-container">
+                        <div className="post-header-icon">
+                            <IonIcon icon={personCircleOutline} size="large"></IonIcon>
+                        </div>
+                        <div className="post-header-name">
+                            <IonCardSubtitle color="secondary">John Smith</IonCardSubtitle>
+                        </div>
                     </div>
-                    <div className="post-header-name">
-                        <IonCardSubtitle color="secondary">John Smith</IonCardSubtitle>
-                    </div>
-                </div>
+                </IonItem>
             </IonCardHeader>
             <IonCardContent>
-                <IonCardSubtitle>This is a post!</IonCardSubtitle>
-                <IonIcon icon={barbellOutline} size="large"/>
-                <IonIcon icon={bicycleOutline} size="large"/>
-                <IonIcon icon={barbellOutline} size="large"/>
-                <IonIcon icon={bicycleOutline} size="large"/>
-                <IonIcon icon={barbellOutline} size="large"/>
-                <IonIcon icon={bicycleOutline} size="large"/>
-                <IonIcon icon={barbellOutline} size="large"/>
-
+                <IonCardSubtitle style={{paddingTop: "10px", paddingBottom: "5px"}}>This is a post!</IonCardSubtitle>
+                <IonIcon icon={barbellOutline} size="large" />
+                <IonIcon icon={bicycleOutline} size="large" />
+                <IonIcon icon={barbellOutline} size="large" />
+                <IonIcon icon={bicycleOutline} size="large" />
+                <IonIcon icon={barbellOutline} size="large" />
+                <IonIcon icon={bicycleOutline} size="large" />
             </IonCardContent>
         </IonCard>
     )
