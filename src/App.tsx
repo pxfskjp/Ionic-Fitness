881970f3683dde8e3e42029b5508d33c408d7668
topdeveloper0729 <topdev0729@gmail.com>
2020-08-07 08:34:07 +1200

    Changed icon for store

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ import {
   IonTabs
 } from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
-import { ellipse, square, triangle } from 'ionicons/icons';
+import { ellipse, cart, triangle } from 'ionicons/icons';
 import Tab1 from './pages/Tab1';
 import Tab2 from './pages/Tab2';
 import Store from './pages/Store';
@@ -54,7 +54,7 @@ const App: React.FC = () => (
             <IonLabel>Tab 2</IonLabel>
           </IonTabButton>
           <IonTabButton tab="store" href="/store">
-            <IonIcon icon={square} />
+            <IonIcon icon={cart} />
             <IonLabel>Store</IonLabel>
           </IonTabButton>
         </IonTabBar>
